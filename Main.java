d461405aba50b1e89bdf2b5ac61da671d629d674
EmmanouilGeorgoudis <emmanouilmusic@gmail.com>
2025-10-06 14:55:38 +0200

    Test3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("Syns jag nu?");
+        System.out.println("Commita och pusha hela dan, yeah");
     }
 }
