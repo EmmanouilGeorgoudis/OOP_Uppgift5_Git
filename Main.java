c51522d2b6e666b78f1a2968fbe03c180d1bb8f4
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2025-10-07 09:46:32 +0200

    mer push

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Syns jag nu?");
         System.out.println("Commita och pusha hela dan, yeah");
         System.out.println("Funkar det fortfarande idag?");
+        System.out.println("Mer push");
     }
 }
+
